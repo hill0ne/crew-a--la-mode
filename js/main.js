41f17e6c47bcd38f566bc2eb8dadff0846552417
hill0ne <jeanj531@gmail.com>
2022-07-07 18:01:52 +0900

    Feat: "추가 header 디자인 변경 애니메이션"

--- js/main.js
+++ js/main.js
@@ -1,23 +1,39 @@
+const aTag = document.querySelectorAll("a");
 const textFixed = document.querySelector(".textwrap-fixed");
+const header = document.querySelector(".header_wrap");
 let scroll = 0;
+
+//  a 요소 기본 이벤트 제거
+for (a of aTag) {
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+}
 
 if (document.readyState) {
   window.addEventListener("scroll", () => {
     scroll = document.documentElement.scrollTop;
-    movePosition();
+    fixTextPosition();
+    fixHeaderPosition();
   });
 }
 
-function movePosition() {
-  console.log(scroll);
+function fixTextPosition() {
   if (scroll > 1250) {
     textFixed.firstElementChild.classList.add("is-active");
     if (scroll > 2700) {
       textFixed.firstElementChild.classList.remove("is-active");
     }
   } else {
-    console.log("else");
     textFixed.firstElementChild.classList.remove("is-active");
+  }
+}
+
+function fixHeaderPosition() {
+  if (scroll > 80) {
+    header.classList.add("is-active");
+  } else {
+    header.classList.remove("is-active");
   }
 }
 
