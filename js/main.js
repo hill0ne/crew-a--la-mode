43bffa43891299ffd1b6ff73bf6cd5bb8507bf32
hill0ne <jeanj531@gmail.com>
2022-07-08 13:58:34 +0900

    Feat: "추가 스크롤, hover 효과"
    - 지정한 영역만큼 스크롤 시 이미지에 텍스트가 나타나는 효과
    - 마우스 hover 시 텍스트가 나타나는 효과

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const aTag = document.querySelectorAll("a");
 const textFixed = document.querySelector(".textwrap-fixed");
 const header = document.querySelector(".header_wrap");
+const list = document.querySelector(".list");
 let scroll = 0;
 
 //  a 요소 기본 이벤트 제거
@@ -15,6 +16,7 @@ if (document.readyState) {
     scroll = document.documentElement.scrollTop;
     fixTextPosition();
     fixHeaderPosition();
+    showImageText();
   });
 }
 
@@ -34,6 +36,35 @@ function fixHeaderPosition() {
     header.classList.add("is-active");
   } else {
     header.classList.remove("is-active");
+  }
+}
+
+function showImageText() {
+  let li = list.children;
+  let a;
+  for (item of li) {
+    a = item.firstElementChild;
+    a.classList.remove("on");
+  }
+  if (scroll > 1250) {
+    // a.classList.remove("on");
+    li[0].firstElementChild.classList.add("on");
+    if (scroll > 1650) {
+      li[0].firstElementChild.classList.remove("on");
+      li[1].firstElementChild.classList.add("on");
+      if (scroll > 2050) {
+        li[1].firstElementChild.classList.remove("on");
+        li[2].firstElementChild.classList.add("on");
+        console.log("li 2번째");
+        if (scroll > 2550) {
+          li[2].firstElementChild.classList.remove("on");
+          li[3].firstElementChild.classList.add("on");
+          if (scroll > 2900) {
+            li[3].firstElementChild.classList.remove("on");
+          }
+        }
+      }
+    }
   }
 }
 
